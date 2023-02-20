fc8629cc05501eaae8fb39a7db22830a90380733
craigkelleher <kelleher.craig@gmail.com>
2023-02-20 09:19:55 -0600

    Implement the class SquareAndSumInput to pass the JUnit tests.

--- Warmups/src/com/galvanize/SquareAndSumtInput.java
+++ Warmups/src/com/galvanize/SquareAndSumtInput.java
@@ -3,7 +3,14 @@ package com.galvanize;
 public class SquareAndSumtInput {
     public static int squareSum(int[] n)
     {
-        //Your code
-        return 0;
+        // Initialize accumulator
+        int sum = 0;
+        // loop through array
+        for (int number : n) {
+            // square each element and add it to the accumulator
+            sum += number * number;
+        }
+        // return the sum of the squares of all numbers in the input array
+        return sum;
     }
 }
