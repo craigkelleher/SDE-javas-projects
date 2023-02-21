da8d2f072a52f5fcbe0e70663fafb57e2ca8d499
craigkelleher <kelleher.craig@gmail.com>
2023-02-21 09:49:15 -0600

    Implement class to pass the tests

--- Warmups/src/com/galvanize/CreditCardMask.java
+++ Warmups/src/com/galvanize/CreditCardMask.java
@@ -2,14 +2,18 @@ package com.galvanize;
 
 public class CreditCardMask {
     public static String maskify(String str) {
+        // return str if input is length 4 or less
         if (str.length() <= 4) {
             return str;
         }
+        // Save the last four characters of the input string into a new string
         String lastFour = str.substring(str.length() - 4);
+        // Initialize an empty string for the masked characters
         StringBuilder masked = new StringBuilder();
-        for (int i = 0; i < str.length() - 4; i++) {
-            masked.append("#");
-        }
+        // loop over the input string, up to the last 4 characters
+        // add a # character to the masked string for each char in the string
+        masked.append("#".repeat(str.length() - 4));
+        // concatenate the masked string and the last four characters
         return masked + lastFour;
     }
 
