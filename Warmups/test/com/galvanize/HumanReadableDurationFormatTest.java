4622c180c8d5be61d084653022562d59afa00267
craigkelleher <kelleher.craig@gmail.com>
2023-02-22 09:39:43 -0600

    Create tests for a program that formats a duration in a human-friendly way

---
+++ Warmups/test/com/galvanize/HumanReadableDurationFormatTest.java
@@ -0,0 +1,16 @@
+package com.galvanize;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HumanReadableDurationFormatTest {
+    @Test
+    public void exampleTests() {
+        assertEquals("1 second", HumanReadableDurationFormat.formatDuration(1));
+        assertEquals("1 minute and 2 seconds", HumanReadableDurationFormat.formatDuration(62));
+        assertEquals("2 minutes", HumanReadableDurationFormat.formatDuration(120));
+        assertEquals("1 hour", HumanReadableDurationFormat.formatDuration(3600));
+        assertEquals("1 hour, 1 minute and 2 seconds", HumanReadableDurationFormat.formatDuration(3662));
+    }
+}
