40879e2295dcb309cb5b20432f68463155763224
craigkelleher <kelleher.craig@gmail.com>
2023-02-27 10:14:50 -0600

    Implement the method that passes the tests to determine if a string of braces is valid "(){}[]"   =>  True "([{}])"   =>  True "(}"       =>  False "[(])"     =>  False "[({})](]" =>  False

---
+++ Warmups/src/com/galvanize/BraceChecker.java
@@ -0,0 +1,52 @@
+package com.galvanize;
+
+import java.util.Stack;
+
+public class BraceChecker {
+
+//    public boolean isValid(String braces) {
+//        int count = 0;
+//        for (int i = 0; i < braces.length(); i++) {
+//            char c = braces.charAt(i);
+//            if (c == '(' || c == '[' || c == '{') {
+//                count++;
+//            } else if (c == ')' || c == ']' || c == '}') {
+//                count--;
+//            }
+//            if (count < 0) {
+//                return false;
+//            }
+//        }
+//        return count == 0;
+//    }
+
+
+    // Create a new stack to keep track of opening braces
+    // loop through each character in input string:
+        // if character is opening brace, push onto stack
+        // if the character is a closing brace
+            // if stack is empty, return false, no matching
+            // pop the most recent opening brace from stack
+            // compare popped opening brace to current closing brace to see if match, if no match, return false
+    // check if stack is empty. If it is, return true because all braces matched
+    // if stack is not empty, return false as atleast one not matched.
+
+    public boolean isValidStack(String braces){
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < braces.length(); i++) {
+            char c = braces.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                if (stack.empty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') || (c == ']' && top != '[') || (c == '}' && top != '{')) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
