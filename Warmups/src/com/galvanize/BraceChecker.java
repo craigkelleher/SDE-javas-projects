cb150c54ea756af96e4685aa158fe2404c4ed71d
craigkelleher <kelleher.craig@gmail.com>
2023-02-28 10:36:53 -0600

    Implement the method that passes the tests to determine if a string of braces is valid "(){}[]"   =>  True "([{}])"   =>  True "(}"       =>  False "[(])"     =>  False "[({})](]" =>  False

--- Warmups/src/com/galvanize/BraceChecker.java
+++ Warmups/src/com/galvanize/BraceChecker.java
@@ -8,7 +8,6 @@ public class BraceChecker {
         // if character is opening brace, push onto stack
         // if the character is a closing brace
             // if stack is empty, return false, no matching
-            // pop the most recent opening brace from stack
             // compare popped opening brace to current closing brace to see if match, if no match, return false
     // check if stack is empty. If it is, return true because all braces matched
     // return whether stack is empty | false  if not as at least one not matched.
@@ -34,22 +33,27 @@ public class BraceChecker {
         return stack.empty();
     }
 
-    // while loop through the string loop while there are still balanced together braces
-    // when there are (inside the loop) remove all instances of balanced braces that are together with ""
-    // {} () []
-    // when done looping check the length of the remaining string
-    // return true if the length is zero
+    // 1. while loop through the string loop while there are still balanced together braces
+    // 2. when there are (inside the loop) remove all instances of balanced braces that are together with ""
+    // 3. when done looping check the length of the remaining string
+    // 4. return true if the length is zero
 
     public boolean isValid(String braces){
         while(braces.contains("{}") || braces.contains("()") || braces.contains("[]")){
-            braces = braces.replace("{}", "").replace("()", "").replace("[]", "");
+            braces = braces
+                    .replace("{}", "")
+                    .replace("()", "")
+                    .replace("[]", "");
         }
         return braces.length() == 0;
     }
 
     public boolean isValid2(String braces){
         for(int i = 0; i < braces.length(); i++){
-            braces = braces.replace("{}", "").replace("()", "").replace("[]", "");
+            braces = braces
+                    .replace("{}", "")
+                    .replace("()", "")
+                    .replace("[]", "");
         }
         return braces.isEmpty();
     }
