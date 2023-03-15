e064e720fb8ee8ea1de67c073f6a3a27bfee5942
craigkelleher <kelleher.craig@gmail.com>
2023-03-15 09:53:56 -0500

    Implement a program that converts a String with - or _ between words to be camelCase.

--- Warmups/src/com/galvanize/ConvertStringCamelCase.java
+++ Warmups/src/com/galvanize/ConvertStringCamelCase.java
@@ -1,5 +1,7 @@
 package com.galvanize;
 
+import java.util.Arrays;
+
 public class ConvertStringCamelCase {
     static String toCamelCase(String s){
         // split the input string into an array of words using _ and - as delimiters
@@ -21,5 +23,34 @@ public class ConvertStringCamelCase {
         }
         // return the result as a String
         return result.toString();
+    }
+
+    static String toCamelCaseStream(String str){
+        String[] words = str.split("[-_]");
+        return Arrays.stream(words, 1, words.length)
+                .map(s -> s.substring(0, 1).toUpperCase() + s.substring(1))
+                .reduce(words[0], String::concat);
+    }
+
+    static String toCamelCaseCharArray(String s){
+        // initialize a StringBuilder
+        StringBuilder camelCaseString = new StringBuilder();
+        // Iterate over the input String
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            // check if current character is a - or a _
+            if (c == '-' || c == '_') {
+                // if it is, increment i
+                i++;
+                // if i < length of the String, upperCase current character
+                if (i < s.length()) {
+                    c = Character.toUpperCase(s.charAt(i));
+                }
+            }
+            // append character to the camelCaseString
+            camelCaseString.append(c);
+        }
+        // return the StringBuilder as a String
+        return camelCaseString.toString();
     }
 }
--- Warmups/test/com/galvanize/ConvertStringCamelCaseTest.java
+++ Warmups/test/com/galvanize/ConvertStringCamelCaseTest.java
@@ -17,4 +17,31 @@ public class ConvertStringCamelCaseTest {
         System.out.println("input: "+input);
         assertEquals("theStealthWarrior", ConvertStringCamelCase.toCamelCase(input));
     }
+
+    @Test
+    public void testSomeUnderscoreLowerStartStrean() {
+        String input = "the_Stealth_Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", ConvertStringCamelCase.toCamelCaseStream(input));
+    }
+    @Test
+    public void testSomeDashLowerStartStream() {
+        String input = "the-Stealth-Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", ConvertStringCamelCase.toCamelCaseStream(input));
+    }
+
+    @Test
+    public void testSomeUnderscoreLowerStartCharArray() {
+        String input = "the_Stealth_Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", ConvertStringCamelCase.toCamelCaseCharArray(input));
+    }
+
+    @Test
+    public void testSomeDashLowerStartCharArray() {
+        String input = "the-Stealth-Warrior";
+        System.out.println("input: "+input);
+        assertEquals("theStealthWarrior", ConvertStringCamelCase.toCamelCaseCharArray(input));
+    }
 }
