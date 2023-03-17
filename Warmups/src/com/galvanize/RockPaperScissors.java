baf52a2d46e6180a816f7fb1a5d8f36454ad9b78
craigkelleher <kelleher.craig@gmail.com>
2023-03-17 09:59:29 -0500

    Implement a program that returns which player wins in a Rock Paper Scissors match

--- Warmups/src/com/galvanize/RockPaperScissors.java
+++ Warmups/src/com/galvanize/RockPaperScissors.java
@@ -3,15 +3,39 @@ package com.galvanize;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+* return Draw if both strings are equal
+* initialize a new hashmap
+* create winning moves by putting them into the map:
+*  rock, scissors
+*  scissors, paper
+*  paper, rock
+* Check if winning moves p1 equals p2
+*  if yes, player 1 won
+*  else player 2 won
+* */
+
+/*
+ * return Draw if both strings are equal
+ * Check if player 1 won
+ *  if p1 equals "rock" && p2 equals Scissors
+ *   or p1 equals scissors && p2 equals paper
+ *   or p1 equals paper && p2 equals rock,
+ *  return player 1 won
+ * else player 2 won
+ * */
+
 public class RockPaperScissors {
     public static String rpsMap(String p1, String p2) {
-        Map<String, String> winningMoves = new HashMap<>();
-        winningMoves.put("rock", "scissors");
-        winningMoves.put("scissors", "paper");
-        winningMoves.put("paper", "rock");
-
-        return p1.equals(p2) ? "Draw!" : winningMoves.get(p1).equals(p2) ?
-                "Player 1 won!" : "Player 2 won!";
+        if (p1.equals(p2)) {
+            return "Draw!";
+        }
+        Map<String, String> winningMoves = new HashMap<>() {{
+            put("rock", "scissors");
+            put("scissors", "paper");
+            put("paper", "rock");
+        }};
+        return winningMoves.get(p1).equals(p2) ? "Player 1 won!" : "Player 2 won!";
     }
 
     public static String rpsConditional(String p1, String p2) {
