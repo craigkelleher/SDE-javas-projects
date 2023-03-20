f7443992f39cde8d93876a30450e59c26a5e6134
craigkelleher <kelleher.craig@gmail.com>
2023-03-20 10:17:23 -0500

    Implement a program that subtracts one list from another and returns the result

--- Warmups/src/com/galvanize/ArrayDifference.java
+++ Warmups/src/com/galvanize/ArrayDifference.java
@@ -1,8 +1,63 @@
 package com.galvanize;
 
+/*
+*  initialize empty list called result
+*  loop through each element in list a
+*   if element is not present in list b, append to result
+* return result
+* */
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ArrayDifference {
     public static int[] arrayDiff(int[] a, int[] b) {
-        // Your code here
-        return a;
+        int[] result = new int[a.length];
+        int count = 0;
+        for (int num : a) {
+            boolean present = false;
+            for (int num2 : b) {
+                if (num == num2) {
+                    present = true;
+                    break;
+                }
+            }
+            if (!present) {
+                result[count] = num;
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
+
+    public static int[] arrayDiffLoop(int[] a, int[] b) {
+        int[] result = new int[a.length];
+        int count = 0;
+        for (int num : a) {
+            int i = 0;
+            while (i < b.length && num != b[i]) {
+                i++;
+            }
+            if (i == b.length) {
+                result[count++] = num;
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
+    /*
+    * 1. Create a list named result, using Arrays.stream method to create a stream of elements in array b
+    * 2. Convert that stream to a list using the boxed and tolist methods.
+    *       This is done so we can use contains method
+    * 3. Create a new Array containing elements of a that are not present in b1 using arrays.stream
+    * method ot create a stream of the elements in array a,
+    * 4. filter method to keep only the elements that are not included in b1
+    * 5. toArray method to convert filtered stream back to an array.
+    * */
+
+    public static int[] arrayDiffStream(int[] a, int[] b) {
+        List<Integer> result = Arrays.stream(b).boxed().toList();
+        return Arrays.stream(a).filter(i -> !result.contains(i)).toArray();
     }
 }
--- Warmups/test/com/galvanize/ArrayDifferenceTest.java
+++ Warmups/test/com/galvanize/ArrayDifferenceTest.java
@@ -13,4 +13,22 @@ class ArrayDifferenceTest {
         assertArrayEquals(new int[] {1,2,2}, ArrayDifference.arrayDiff(new int [] {1,2,2}, new int[] {}));
         assertArrayEquals(new int[] {}, ArrayDifference.arrayDiff(new int [] {}, new int[] {1,2}));
     }
+
+    @Test
+    public void sampleTestsLoop() {
+        assertArrayEquals(new int[] {2}, ArrayDifference.arrayDiffLoop(new int [] {1,2}, new int[] {1}));
+        assertArrayEquals(new int[] {2,2}, ArrayDifference.arrayDiffLoop(new int [] {1,2,2}, new int[] {1}));
+        assertArrayEquals(new int[] {1}, ArrayDifference.arrayDiffLoop(new int [] {1,2,2}, new int[] {2}));
+        assertArrayEquals(new int[] {1,2,2}, ArrayDifference.arrayDiffLoop(new int [] {1,2,2}, new int[] {}));
+        assertArrayEquals(new int[] {}, ArrayDifference.arrayDiffLoop(new int [] {}, new int[] {1,2}));
+    }
+
+    @Test
+    public void sampleTestsStream() {
+        assertArrayEquals(new int[] {2}, ArrayDifference.arrayDiffStream(new int [] {1,2}, new int[] {1}));
+        assertArrayEquals(new int[] {2,2}, ArrayDifference.arrayDiffStream(new int [] {1,2,2}, new int[] {1}));
+        assertArrayEquals(new int[] {1}, ArrayDifference.arrayDiffStream(new int [] {1,2,2}, new int[] {2}));
+        assertArrayEquals(new int[] {1,2,2}, ArrayDifference.arrayDiffStream(new int [] {1,2,2}, new int[] {}));
+        assertArrayEquals(new int[] {}, ArrayDifference.arrayDiffStream(new int [] {}, new int[] {1,2}));
+    }
 }
